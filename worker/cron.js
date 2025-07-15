73f4b5706178c91270b2425777ec9f35489356a5
Vincent Will <vincent.will@onlogist.com>
2025-07-15 15:41:20 +0200

    init docker setup and add cron

--- worker/cron.js
+++ worker/cron.js
@@ -1,8 +1,9 @@
 import cron from 'node-cron'
+import { run } from './index.js'
 
 console.log('Init cron job')
 
 // once a day
-cron.schedule('0 10 * * *', async () => {
-    // TODO
+cron.schedule('0 4 * * *', async () => {
+    await run()
 })
--- worker/index.js
+++ worker/index.js
@@ -7,28 +7,30 @@ import { runLighthouseCheck } from './checks/lighthouse.js'
 import { runPerformanceCheck } from './checks/performance.js'
 import { runCustomChecks } from './checks/custom.js'
 
-try {
-  const db = await connectDB()
-  const users = await db.collection('users').find({}).toArray()
+export const run = async () => {
+  try {
+    const db = await connectDB()
+    const users = await db.collection('users').find({}).toArray()
 
-  for (const user of users) {
-    const createdAt = new Date().toISOString()
-    console.log(createdAt, 'run status check for', user.domain)
+    for (const user of users) {
+      const createdAt = new Date().toISOString()
+      console.log(createdAt, 'run status check for', user.domain)
 
-    // await runUptimeCheck({ uri: user.domain, db, userId: user._id, createdAt })
-    // await runFuzzCheck({ uri: user.domain, db, userId: user._id, createdAt })
-    // await runHeaderCheck({ uri: user.domain, db, userId: user._id, createdAt })
-    // await runLighthouseCheck({ uri: user.domain, db, userId: user._id, createdAt })
-    // await runPerformanceCheck({ uri: user.domain, db, userId: user._id, createdAt })
-    await runCustomChecks({ uri: user.domain, db, userId: user._id, createdAt })
+      await runUptimeCheck({ uri: user.domain, db, userId: user._id, createdAt })
+      await runFuzzCheck({ uri: user.domain, db, userId: user._id, createdAt })
+      await runHeaderCheck({ uri: user.domain, db, userId: user._id, createdAt })
+      await runLighthouseCheck({ uri: user.domain, db, userId: user._id, createdAt })
+      await runPerformanceCheck({ uri: user.domain, db, userId: user._id, createdAt })
+      await runCustomChecks({ uri: user.domain, db, userId: user._id, createdAt })
 
-    // run other checks
-    // SSL https://www.npmjs.com/package/node-ssllabs
-    
-    console.log('finished all checks')
+      // run other checks
+      // SSL https://www.npmjs.com/package/node-ssllabs
+
+      console.log('finished all checks')
+    }
+  } catch (e) {
+    console.error('unexpected error', e)
+  } finally {
+    await disconnectDB()
   }
-} catch (e) {
-  console.error('unexpected error', e)
-} finally {
-  await disconnectDB()
 }
