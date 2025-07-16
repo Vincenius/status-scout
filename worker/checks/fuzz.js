9952cd2b52866e060ad23f294a12b57bce30961f
Vincent Will <vincent.will@onlogist.com>
2025-07-16 08:56:17 +0200

    use p-limit for batches

--- worker/checks/fuzz.js
+++ worker/checks/fuzz.js
@@ -1,14 +1,15 @@
 import fs from 'fs'
 import path from 'path'
+import pLimit from 'p-limit';
 import { createCheckResult } from '../db.js'
 
 function splitIntoBatches(arr, batchSize) {
   let result = [];
-  
+
   for (let i = 0; i < arr.length; i += batchSize) {
     result.push(arr.slice(i, i + batchSize));
   }
-  
+
   return result;
 }
 
@@ -17,27 +18,29 @@ export const runFuzzCheck = async ({ uri, db, userId, createdAt }) => {
   const fuzzFile = fs.readFileSync(fuzzPath).toString() // https://github.com/Bo0oM/fuzz.txt
   const files = fuzzFile.split('\n')
   // split into batches
-  const batches = splitIntoBatches(files, 20);
-  const statusCodes = []
-  let i = 0;
-  for (const batch of batches) {
-    console.debug(`Batch ${i++} of ${batches.length}`)
-    const results = await Promise.all(
-      batch.map(async (file) => {
-        const filename = file.startsWith('/') ? file.slice(1) : file;
-        const res = await fetch(`${uri}/${filename}`, { method: 'head' })
-        return { status: res.status, file }
-      })
-    )
-    statusCodes.push(...results)
-  }
 
-  const availableFiles = statusCodes.filter(sc => sc.status !== 404 && sc.status !== 403)
+  const limit = pLimit(20); // max 10 concurrent requests
+  const promises = files.map(file =>
+    limit(async () => {
+      try {
+        const filename = file.startsWith('/') ? file.slice(1) : file;
+        const res = await fetch(`${uri}/${filename}`, { method: 'head' });
+      return { status: res.status, file };
+      } catch (err) {
+        console.log('fetch error', file, err)
+        return { status: 500, file };
+      }
+    })
+  );
+
+  const results = await Promise.all(promises);
+
+  const availableFiles = results.filter(sc => sc.status !== 404 && sc.status !== 403)
   const result = {
     status: availableFiles.length === 0 ? 'success' : 'fail',
     details: {
       files: availableFiles
-    }, 
+    },
   }
 
   await createCheckResult({ db, userId, createdAt, check: 'fuzz', result })
