208b3045d81cab2f194882c87287fb13a066411b
Vincent Will <vincent.will@onlogist.com>
2025-07-17 14:05:00 +0200

    design updates

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,26 +1,66 @@
 import Layout from '@/components/Layout/Layout'
 import useSWR from 'swr'
-import { Box, Card, Flex, SimpleGrid, Table, Text, ThemeIcon, Title, Tooltip, LoadingOverlay } from '@mantine/core'
-import { IconAccessible, IconBrandSpeedtest, IconChartBar, IconShieldLock, IconZoomCode } from '@tabler/icons-react'
+import { Box, Card, Flex, SimpleGrid, Table, Text, ThemeIcon, Title, Tooltip, LoadingOverlay, Divider } from '@mantine/core'
+import { IconAccessible, IconBrandSpeedtest, IconChartBar, IconListCheck, IconShieldLock, IconZoomCode } from '@tabler/icons-react'
 import fetcher from '@/utils/fetcher'
+import { useMediaQuery } from '@mantine/hooks'
 
 const Chart = ({ data = [] }) => {
-  console.log(data)
+  // figure out current screen size
+  const isXxs = useMediaQuery('(max-width: 24em)');
+  const isXs = useMediaQuery('(max-width: 36em)');
+  const isSm = useMediaQuery('(max-width: 48em)');
+  const isMd = useMediaQuery('(max-width: 62em)');
+  const isLg = useMediaQuery('(max-width: 75em)');
+  const isXl = useMediaQuery('(max-width: 88em)');
+
+  let visibleCount = 40;
+  if (isXxs) visibleCount = 20;
+  else if (isXs) visibleCount = 30;
+  else if (isSm) visibleCount = 40;
+  else if (isMd) visibleCount = 20;
+  else if (isLg) visibleCount = 30;
+
+  console.log(visibleCount)
+
+  // take only last N items
+  const visibleData = data.slice(-visibleCount);
+
+  // pad up to visibleCount
+  const paddedData = [
+    ...Array.from({ length: Math.max(visibleCount - visibleData.length, 0) }, () => null),
+    ...visibleData,
+  ];
+
   return (
-    <Flex gap="2px">
-      {data.length < 30 ? Array.from({ length: 30 - data.length }).map((_, index) => (
-        <Card key={index} h={20} w={10} p="0" bg="white" withBorder></Card>
-      )) : <></>}
-      {
-        data.map((item, index) => (
+    <SimpleGrid
+      spacing={3}
+      cols={visibleCount}
+    >
+      {paddedData.map((item, index) =>
+        item ? (
           <Tooltip key={index} label={new Date(item.createdAt).toLocaleString()}>
-            <Card h={20} w={10} p="0" bg={item.result.status === 'success' ? 'green' : 'red'}></Card>
+            <Card
+              h={40}
+              w={10}
+              p="0"
+              bg={item.result.status === 'success' ? 'green' : 'red'}
+            />
           </Tooltip>
-        ))
-      }
-    </Flex>
-  )
-}
+        ) : (
+          <Card
+            key={index}
+            h={40}
+            w={10}
+            p="0"
+            bg="white"
+            withBorder
+          />
+        )
+      )}
+    </SimpleGrid>
+  );
+};
 
 function Dashboard() {
   const { data = [], error, isLoading } = useSWR(`${import.meta.env.VITE_API_URL}/v1/user`, fetcher)
@@ -34,8 +74,6 @@ function Dashboard() {
       </Layout>
     )
   }
-
-  console.log(data, flows)
 
   return (
     <Layout title="Dashboard">
@@ -56,88 +94,101 @@ function Dashboard() {
       </Box> */}
 
       <SimpleGrid cols={{ xs: 1, sm: 2 }} mb="md">
-        <Card withBorder shadow="md">
+        <Card withBorder shadow="md" maw={720}>
           <Title mb="md" order={2} size="h4" fw="normal">Status Checks</Title>
-          <Table>
-            <Table.Tbody>
-              <Table.Tr>
-                <Table.Td>
-                  <Flex gap="xs" align="center">
-                    <ThemeIcon variant="default" size="sm">
-                      <IconChartBar style={{ width: '70%', height: '70%' }} />
-                    </ThemeIcon>
-                    <Text>Uptime</Text>
-                  </Flex>
-                </Table.Td>
-                <Table.Td><Chart data={data.filter(d => d.check === 'uptime')} /></Table.Td>
-              </Table.Tr>
-              <Table.Tr>
-                <Table.Td>
-                  <Flex gap="xs" align="center">
-                    <ThemeIcon variant="default" size="sm">
-                      <IconShieldLock style={{ width: '70%', height: '70%' }} />
-                    </ThemeIcon>
-                    <Text>Security</Text>
-                  </Flex>
-                </Table.Td>
-                <Table.Td><Chart data={data.filter(d => d.check === 'fuzz')} /></Table.Td>
-              </Table.Tr>
-              <Table.Tr>
-                <Table.Td>
-                  <Flex gap="xs" align="center">
-                    <ThemeIcon variant="default" size="sm">
-                      <IconAccessible style={{ width: '70%', height: '70%' }} />
-                    </ThemeIcon>
-                    <Text>Accessibility</Text>
-                  </Flex>
-                </Table.Td>
-                <Table.Td><Chart data={data.filter(d => d.check === 'a11y')} /></Table.Td>
-              </Table.Tr>
-              <Table.Tr>
-                <Table.Td>
-                  <Flex gap="xs" align="center">
-                    <ThemeIcon variant="default" size="sm">
-                      <IconZoomCode style={{ width: '70%', height: '70%' }} />
-                    </ThemeIcon>
-                    <Text>SEO</Text>
-                  </Flex>
-                </Table.Td>
-                <Table.Td><Chart data={data.filter(d => d.check === 'seo')} /></Table.Td>
-              </Table.Tr>
-              <Table.Tr>
-                <Table.Td>
-                  <Flex gap="xs" align="center">
-                    <ThemeIcon variant="default" size="sm">
-                      <IconBrandSpeedtest style={{ width: '70%', height: '70%' }} />
-                    </ThemeIcon>
-                    <Text>Performance</Text>
-                  </Flex>
-                </Table.Td>
-                <Table.Td><Chart data={data.filter(d => d.check === 'performance')} /></Table.Td>
-              </Table.Tr>
-            </Table.Tbody>
-          </Table>
+
+          <Box>
+            <Flex gap="xs" align="center" mb="xs">
+              <ThemeIcon variant="white" size="md">
+                <IconChartBar style={{ width: '70%', height: '70%' }} />
+              </ThemeIcon>
+              <Text size="sm">Uptime</Text>
+            </Flex>
+
+            <Chart data={data.filter(d => d.check === 'uptime')} />
+          </Box>
+
+          <Divider my="sm" />
+
+          <Box>
+            <Flex gap="xs" align="center" mb="xs">
+              <ThemeIcon variant="white" size="md">
+                <IconShieldLock style={{ width: '70%', height: '70%' }} />
+              </ThemeIcon>
+              <Text size="sm">Security</Text>
+            </Flex>
+
+            <Chart data={data.filter(d => d.check === 'fuzz')} />
+          </Box>
+
+          <Divider my="sm" />
+
+          <Box>
+            <Flex gap="xs" align="center" mb="xs">
+              <ThemeIcon variant="white" size="md">
+                <IconAccessible style={{ width: '70%', height: '70%' }} />
+              </ThemeIcon>
+              <Text size="sm">Accessibility</Text>
+            </Flex>
+
+            <Chart data={data.filter(d => d.check === 'a11y')} />
+          </Box>
+
+          <Divider my="sm" />
+
+          <Box>
+            <Flex gap="xs" align="center" mb="xs">
+              <ThemeIcon variant="white" size="md">
+                <IconZoomCode style={{ width: '70%', height: '70%' }} />
+              </ThemeIcon>
+              <Text size="sm">SEO</Text>
+            </Flex>
+
+            <Chart data={data.filter(d => d.check === 'seo')} />
+          </Box>
+
+
+          <Divider my="sm" />
+
+          <Box>
+            <Flex gap="xs" align="center" mb="xs">
+              <ThemeIcon variant="white" size="md">
+                <IconBrandSpeedtest style={{ width: '70%', height: '70%' }} />
+              </ThemeIcon>
+              <Text size="sm">Performance</Text>
+            </Flex>
+
+            <Chart data={data.filter(d => d.check === 'performance')} />
+          </Box>
         </Card>
-        <Card withBorder shadow="md">
+        <Card withBorder shadow="md" maw={720}>
           <Title mb="md" order={2} size="h4" fw="normal">Custom Flows</Title>
           <Table>
             <Table.Tbody>
               {flows.map((item, index) => (
-                <Table.Tr key={index}>
-                  <Table.Td>
-                    <Flex gap="xs" align="center">
-                      <Text>{item.name}</Text>
+                <Box key={index}>
+                  <Box pb="xs">
+                    <Flex gap="xs" align="center" mb="xs">
+                      <ThemeIcon variant="white" size="md">
+                        <IconListCheck style={{ width: '70%', height: '70%' }} />
+                      </ThemeIcon>
+                      <Text size="sm">{item.name}</Text>
                     </Flex>
-                  </Table.Td>
-                  <Table.Td>
+
+
                     <Chart
                       data={data
                         .filter(d => d.check === 'custom')
-                        .map(c => c.result.find(r => r.name === item.name))
+                        .map(c => ({
+                          createdAt: c.createdAt,
+                          ...c.result.find(r => r.name === item.name),
+                        }))
                         .filter(Boolean)}
                     />
-                  </Table.Td>
-                </Table.Tr>
+                  </Box>
+
+                  {index !== flows.length - 1 && <Divider my="sm" />}
+                </Box>
               ))}
             </Table.Tbody>
           </Table>
