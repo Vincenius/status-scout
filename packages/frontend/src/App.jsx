f29d946b68cd89470dcb73b75b65626079921cd0
Vincent Will <vincent.will@onlogist.com>
2025-08-18 08:42:46 +0200

    update 404 logic

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -4,13 +4,21 @@ import "./global.css";
 
 import { Center, Loader, MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
-import { useRoutes } from 'react-router-dom';
+import { useRoutes, Navigate } from 'react-router-dom';
 import routes from '~react-pages';
 import { Suspense, lazy } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 
+
 function AppRoutes() {
-  return useRoutes(routes);
+  const element = useRoutes([
+    ...routes,
+    {
+      path: "*",
+      element: <Navigate to="/404.html" replace />
+    }
+  ]);
+  return element;
 }
 
 export default function App() {
