f22dba5e83982ca4704473aad673fbc26a7f23fa
Vincent Will <vincent.will@onlogist.com>
2025-08-28 12:53:37 +0200

    add debug log

--- packages/frontend/src/components/Layout/Layout.jsx
+++ packages/frontend/src/components/Layout/Layout.jsx
@@ -6,6 +6,7 @@ import ColorSchemeToggle from './ColorSchemeToggle.jsx';
 import { useEffect } from 'react';
 
 const Layout = ({ children, title, hideNav }) => {
+  console.log('VITE_ENABLE_ANALYTICS:', import.meta.env.VITE_ENABLE_ANALYTICS);
   const isAnalyticsEnabled = import.meta.env.VITE_ENABLE_ANALYTICS === 'true' || import.meta.env.VITE_ENABLE_ANALYTICS === true;
 
   // set head info on initial load
